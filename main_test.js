b5dbd8c9ae9f638968b868e82471f8ac74c3232d
EduardoPhilippeCosta <epcdudu@gmail.com>
2022-10-10 19:25:15 -0300

    leitura arquivo

--- main_test.js
+++ main_test.js
@@ -1,43 +1,16 @@
+const vision = require('@google-cloud/vision');
 
-const vision = require('@google-cloud/vision').v1;
+async function setEndpoint() {
+    const clientOptions = {
+        apiEndpoint: 'eu-vision.googleapis.com',
+        keyFilename: 'C:\\Users\\iamdu\\OneDrive\\Documentos\\Projetos\\deteccaolabel\\key.json'
+    };
 
-const client = new vision.ImageAnnotatorClient({
-    keyFilename: 'C:\\Users\\iamdu\\OneDrive\\Documentos\\Projetos\\deteccaolabel\\key.json'
-});
+    const client = new vision.ImageAnnotatorClient(clientOptions);
 
-/*
-const bucketName = 'my-bucket';
-const fileName = 'path/to/document.pdf';
-const outputPrefix = 'results'
-*/
-
-const gcsSourceUri = `C:\\Users\\iamdu\\Downloads\\invoice-template-us-neat-750px.pdf`;
-const gcsDestinationUri = `C:\\Users\\iamdu\\Downloads\\`;
-
-const inputConfig = {
-    mimeType: 'application/pdf',
-    gcsSource: {
-        uri: gcsSourceUri,
-    },
-};
-const outputConfig = {
-    gcsDestination: {
-        uri: gcsDestinationUri,
-    },
-};
-const features = [{ type: 'DOCUMENT_TEXT_DETECTION' }];
-const request = {
-    requests: [
-        {
-            inputConfig: inputConfig,
-            features: features,
-            outputConfig: outputConfig,
-        },
-    ],
-};
-
-const [operation] = await client.asyncBatchAnnotateFiles(request);
-const [filesResponse] = await operation.promise();
-const destinationUri =
-    filesResponse.responses[0].outputConfig.gcsDestination.uri;
-console.log('Json saved to: ' + destinationUri);
+    const [result] = await client.textDetection('C:\\Users\\iamdu\\Downloads\\cupom-fiscal.jpg');
+    const labels = result.textAnnotations;
+    console.log('Text:');
+    labels.forEach(label => console.log(label.description));
+}
+setEndpoint();
